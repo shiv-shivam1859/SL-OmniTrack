20e5ccee37ae6b908a6b6e2984ec6ee8068dc1bf
Shiv <shivamsinhav1@proton.me>
2026-06-29 09:24:04 +0530

    Added those equations in the code.

--- Firmware/main.cpp
+++ Firmware/main.cpp
@@ -23,8 +23,40 @@ filter.begin(100.0f);
 
 }
 
-void loop(){
+void loop() {
+    
+    if (imu.readSensor()) {
+        float ax = imu.getAccelX_mss();
+        float ay = imu.getAccelY_mss();
+        float az = imu.getAccelZ_mss();
+        
+        float gx = imu.getGyroX_rads();
+        float gy = imu.getGyroY_rads();
+        float gz = imu.getGyroZ_rads();
+        
+        float mx = imu.getMagX_uT();
+        float my = imu.getMagY_uT();
+        float mz = imu.getMagZ_uT();
 
+        
+        filter.update(
+            gx * 180.0 / PI, gy * 180.0 / PI, gz * 180.0 / PI,
+            ax, ay, az,
+            my, mx, -mz  
+        );
 
+        
+        float roll  = filter.getRoll();
+        float pitch = filter.getPitch();
+        float yaw   = filter.getYaw();
 
+      
+        Serial.print("Orientation: ");
+        Serial.print(roll);  Serial.print(", ");
+        Serial.print(pitch); Serial.print(", ");
+        Serial.println(yaw);
+    }
+    
+    
+    delay(10); 
 }
